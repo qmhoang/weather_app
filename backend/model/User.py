16a1aa11c766db71dd90f6357491d516d36b3c57
Quang-Minh Hoang <qhoang@nexatech.com>
2016-04-30 08:41:13 -0500

    v1 backend

--- backend/model/User.py
+++ backend/model/User.py
@@ -1,6 +1,7 @@
-from alchy import ModelBase, make_declarative_base
-from sqlalchemy import orm, Column, types
+from alchy import make_declarative_base
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer)
 from passlib.hash import sha256_crypt
+from sqlalchemy import Column, types, ForeignKey, Enum, UniqueConstraint
 
 Model = make_declarative_base()
 
@@ -10,12 +11,27 @@ class User(Model):
 
     id = Column(types.Integer, primary_key=True)
     username = Column(types.String(80), unique=True)  # probably should be email
-    password_hash = Column(types.String(256))  # needs to be salted in the future, unsalted right now
+    password_hash = Column(types.String(256), nullable=False)  # needs to be salted in the future, unsalted right now
 
     def __init__(self, username, password):
         Model.__init__(self)
         self.username = username
         self.password_hash = sha256_crypt.encrypt(password)
 
+    def set_pass(self, password):
+        self.password_hash = sha256_crypt.encrypt(password)
+
     def verify_pass(self, password):
         return sha256_crypt.verify(password, self.password_hash)
+
+    def gen_auth_token(self, serializer):
+        return serializer.dumps({'id': self.id})
+
+
+class Location(Model):
+    __tablename__ = 'sites'
+
+    user_id = Column(types.Integer, ForeignKey('users.id'), index=True, primary_key=True, nullable=False)
+    address_type = Column(Enum("LAT_LONG", "CITY", "CITY_COUNTRY", "ZIPCODE", name="address_type"), primary_key=True, nullable=False)
+    address = Column(types.String(50), primary_key=True, nullable=False)  # could be zip code, city, lat & long, whatever
+    UniqueConstraint(user_id, address_type, address)
